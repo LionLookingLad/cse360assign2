797466482f830657b33e6fb856795e499fd8dbf2
LionLookingLad <56379041+LionLookingLad@users.noreply.github.com>
2019-10-09 23:11:47 -0700

    AddingMachine
    but this time I looked up what JavaDoc means

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,34 +1,34 @@
 package cse360assign2;
-
+/**keeps track of a total sum */
 public class AddingMachine {
 
 	private int total;
 	private String history;
 	
-	//constructor, initialize total as 0
+	/**constructor, initialize total and history as 0 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 		history = "0";
 	}
 	
-	//should return total
+	/**should return total */
 	public int getTotal () {
 		return total;
 	}
 	
-	//increase total by a new value
+	/**increase total by a new value */
 	public void add (int value) {
 		total += value;
 		history = history + " + " + value;
 	}
 	
-	//decrease total by a new value
+	/**decrease total by a new value */
 	public void subtract (int value) {
 		total -= value;
 		history = history + " - " + value;
 	}
 		
-	//return a string showing all operations done to total thus far
+	/**return a string showing all operations done to total thus far */
 	public String toString () {
 		return history;
 	}
