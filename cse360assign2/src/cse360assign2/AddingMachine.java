1ebb0fec5ff9a6a5799ab3fb946aedabec84b6ff
LionLookingLad <56379041+LionLookingLad@users.noreply.github.com>
2019-10-09 22:43:33 -0700

    Add files via upload

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,35 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	//constructor, initialize total as 0
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//should return total
+	public int getTotal () {
+		return 0;
+	}
+	
+	//increase total by a new value
+	public void add (int value) {
+		
+	}
+	
+	//decrease total by a new value
+	public void subtract (int value) {
+		
+	}
+		
+	//return a string showing all operations done to total thus far
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
