bbec7a88826e8406c4ad408f06e0a5923a740d08
LionLookingLad <56379041+LionLookingLad@users.noreply.github.com>
2019-10-09 22:56:43 -0700

    AddingMachine
    now with code!

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,30 +3,34 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	//constructor, initialize total as 0
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	//should return total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	//increase total by a new value
 	public void add (int value) {
-		
+		total += value;
+		history = history + " + " + value;
 	}
 	
 	//decrease total by a new value
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history + " - " + value;
 	}
 		
 	//return a string showing all operations done to total thus far
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
